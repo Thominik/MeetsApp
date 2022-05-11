2deb1bcbe4dbe5b83464b27a938454a9881dc893
Thominik <dominikbednarz.ti@gmail.com>
2022-05-11 18:10:30 +0200

    Messages not working

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -15,10 +15,10 @@ public class MessageRepository : IMessageRepository
 
     public MessageRepository(DataContext context, IMapper mapper)
     {
-        _context = context;
         _mapper = mapper;
+        _context = context;
     }
-    
+
     public void AddMessage(Message message)
     {
         _context.Messages.Add(message);
@@ -31,7 +31,10 @@ public class MessageRepository : IMessageRepository
 
     public async Task<Message> GetMessage(int id)
     {
-        return await _context.Messages.FindAsync(id);
+        return await _context.Messages
+            .Include(u => u.Sender)
+            .Include(u => u.Recipient)
+            .SingleOrDefaultAsync(x => x.Id == id);
     }
 
     public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -42,9 +45,12 @@ public class MessageRepository : IMessageRepository
 
         query = messageParams.Container switch
         {
-            "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
-            "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
-            _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.DateRead == null)
+            "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username
+                                        && u.RecipientDeleted == false),
+            "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username
+                                         && u.SenderDeleted == false),
+            _ => query.Where(u => u.Recipient.UserName ==
+                messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
         };
 
         var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -52,21 +58,22 @@ public class MessageRepository : IMessageRepository
         return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
+    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername,
+        string recipientUsername)
     {
         var messages = await _context.Messages
             .Include(u => u.Sender).ThenInclude(p => p.Photos)
             .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-            .Where(m => m.Recipient.UserName == currentUsername
-                        && m.Sender.UserName == recipientUsername
+            .Where(m => m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
+                                                                && m.Sender.UserName == recipientUsername
                         || m.Recipient.UserName == recipientUsername
-                        && m.Sender.UserName == currentUsername
+                        && m.Sender.UserName == currentUsername && m.SenderDeleted == false
             )
             .OrderBy(m => m.MessageSent)
             .ToListAsync();
 
-        var unreadMessages =
-            messages.Where(m => m.DateRead == null && m.Recipient.UserName == currentUsername).ToList();
+        var unreadMessages = messages.Where(m => m.DateRead == null
+                                                 && m.Recipient.UserName == currentUsername).ToList();
 
         if (unreadMessages.Any())
         {
